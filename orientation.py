b324f39b05ffee967745b0afe5f4784958f104e6
Konrad <konrad1borowik@gmail.com>
2021-11-11 10:11:45 +0100

    drawAxis function

--- orientation.py
+++ orientation.py
@@ -3,8 +3,30 @@ from imutils import contours
 from skimage import measure
 import imutils
 import numpy as np
-import math
-from PIL import Image
+from math import atan2, cos, sin, sqrt, pi
+
+
+def drawAxis(img, p_, q_, color, scale):
+    p = list(p_)
+    q = list(q_)
+
+    ## [visualization1]
+    angle = atan2(p[1] - q[1], p[0] - q[0])  # angle in radians
+    hypotenuse = sqrt((p[1] - q[1]) * (p[1] - q[1]) + (p[0] - q[0]) * (p[0] - q[0]))
+
+    # Here we lengthen the arrow by a factor of scale
+    q[0] = p[0] - scale * hypotenuse * cos(angle)
+    q[1] = p[1] - scale * hypotenuse * sin(angle)
+    cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv2.LINE_AA)
+
+    # create the arrow hooks
+    p[0] = q[0] + 9 * cos(angle + pi / 4)
+    p[1] = q[1] + 9 * sin(angle + pi / 4)
+    cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv2.LINE_AA)
+
+    p[0] = q[0] + 9 * cos(angle - pi / 4)
+    p[1] = q[1] + 9 * sin(angle - pi / 4)
+    cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv2.LINE_AA)
 
 
 def LedDetector(image):
